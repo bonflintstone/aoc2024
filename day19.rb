95783edc65cbf29d9392049cdae99c5ace0b4260
Bonflintstone <freddy@railslove.com>
2024-12-19 15:21:12 +0100

    :broom:

--- day19.rb
+++ day19.rb
@@ -2,14 +2,7 @@ TOWELS, PATTERNS = File.read('./day19input.txt').split("\n\n").map { _1.split(/\
 
 MEMO = {}
 def test_pattern(pattern)
-  MEMO[pattern] ||= begin
-    return 1 if pattern.empty?
-
-    TOWELS
-      .filter { pattern.start_with? _1 }
-      .map { pattern[_1.length..] }
-      .sum { test_pattern(_1) }
-  end
+  MEMO[pattern] ||= TOWELS.filter { pattern.start_with? _1 }.sum { _1 == pattern ? 1 : test_pattern(pattern[_1.length..] ) }
 end
 
 puts PATTERNS.count { test_pattern(_1).positive? }
