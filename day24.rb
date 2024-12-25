4715fd89a94685a1792d143da6b11b0f5212a134
Bonflintstone <freddy@railslove.com>
2024-12-25 16:41:22 +0100

    Trying more structure

--- day24.rb
+++ day24.rb
@@ -3,7 +3,7 @@ input, logic = File.read('./day24input.txt').split("\n\n")
 GATES = {} # name: Input/ Gate
 
 class Input
-  attr_accessor :tags, :overwrite, :name
+  attr_accessor :tags, :name, :value
 
   def initialize(name, value)
     @name, @value = name, value
@@ -16,85 +16,71 @@ class Input
 
   def upstream = [self]
 
-  def get_output 
-    return overwrite unless overwrite.nil?
-    @value
-  end
+  def get_output = @value
 end
 
 class Gate
-  attr_accessor :tags, :overwrite, :name
+  attr_accessor :tags, :name
 
-  def initialize(name, in1, in2, op)
-    @name, @in1, @in2, @op = name, in1, in2, op
+  def initialize(name, in1, in2, op, gates)
+    @name, @in1, @in2, @op, @gates = name, in1, in2, op, gates
     @tags = []
   end
 
-  def upstream
-    [self, *GATES[@in1].upstream, *GATES[@in2].upstream]
-  end
+  def upstream = [self, *@gates[@in1].upstream, *@gates[@in2].upstream]
 
-  def inspect
-    "<#{@name} o=#{get_output} tags=#{tags.tally} op=#{@op} in1=#{GATES[@in1]} in2=#{GATES[@in2]}>"
-  end
+  def inspect = "<#{@name} o=#{get_output} tags=#{tags.tally} op=#{@op} in1=#{@gates[@in1]} in2=#{@gates[@in2]}>"
 
   def to_s = "<Gate:#{@name} o=#{get_output}>"
 
   def get_output 
-    return overwrite unless overwrite.nil?
-
-    [GATES[@in1], GATES[@in2]].map(&:get_output).reduce({AND: :&, OR: :|, XOR: :^}[@op.to_sym])
+    # puts [@gates[@in1], @gates[@in2]].map(&:get_output)
+    [@gates[@in1], @gates[@in2]].map(&:get_output).reduce({AND: :&, OR: :|, XOR: :^}[@op.to_sym])
   end
 end
 
-input.split("\n").each { g, v = _1.split(': '); GATES[g] = Input.new(g, v.to_i) }
-logic.split("\n").each { g1, op, g2, _, g3 = _1.split; GATES[g3] = Gate.new(g3, g1, g2, op) }
+class Machine
+  def initialize(input, logic)
+    @gates = {}
+    input.split("\n").each { g, v = _1.split(': '); @gates[g] = Input.new(g, v.to_i) }
+    logic.split("\n").each { g1, op, g2, _, g3 = _1.split; @gates[g3] = Gate.new(g3, g1, g2, op, @gates) }
+  end
 
-OUTPUT_GATES = GATES.keys.grep(/^z/).sort.map { GATES[it] }
+  def get_number(char) = @gates.keys.grep(/^#{char}/).sort.map { @gates[it] }.each_with_index.sum { _1.get_output * (2 ** _2) }
 
-def get_number(char) = GATES.keys.grep(/^#{char}/).sort.map { GATES[it] }.each_with_index.sum { _1.get_output * (2 ** _2) }
+  def get_output(n)
+    @gates["z#{n.to_s.rjust(2, '0')}"]
+  end
 
-def get_z = OUTPUT_GATES.each_with_index.sum { _1.get_output * (2 ** _2) }
+  def compute(x = nil, y = nil)
+    @gates.keys.grep(/^x/).sort.each_with_index do |g, i|
+      @gates[g].value = x.to_s(2).reverse[i].to_i
+    end unless x.nil?
 
-SHOULD = (get_number('x') + get_number('y'))
+    @gates.keys.grep(/^y/).sort.each_with_index do |g, i|
+      @gates[g].value = y.to_s(2).reverse[i].to_i
+    end unless y.nil?
 
-puts SHOULD.to_s(2)
-
-SHOULD.to_s(2).chars.map(&:to_i).reverse.each_with_index do |bit, i|
-  gate = GATES["z#{i.to_s.rjust(2, '0')}"]
-  tag = gate.get_output == bit ? :good : :bad
-  gate.upstream.each { it.tags << tag }
-end
-
-bad_true, bad_false = GATES.values.sort_by { -it.tags.count(:bad) }.filter { it.tags.include?(:bad) }.partition { it.get_output != 0 }
-
-def score(result) = SHOULD.to_s(2).chars.zip(result.to_s(2).chars).filter { _1 != _2 }.count
-
-DEFAULT_SCORE = score(get_number('z'))
-
-def reset = GATES.values.each { it.overwrite = nil }
-
-def score_swap(swaps)
-  reset
-  swaps.each { it[0].overwrite = it[1] }
-  score(get_number('z'))
-end
-
-bad_true.filter! { score_swap([[it, 0]]) < DEFAULT_SCORE - 1 }
-bad_false.filter! { score_swap([[it, 1]]) < DEFAULT_SCORE - 1 }
-
-best_score = 1000
-bad_true.combination(4).each_with_index do |truths, i|
-  # puts [i, best_score].to_s
-  bad_false.combination(4).each do |falses|
-    score = score_swap((truths + falses).zip([0, 0, 0, 0, 1, 1, 1, 1]))
-
-    best_score = [best_score, score].min
-
-    if score == 1
-      puts get_number('z').to_s(2)
-      # binding.irb
-      # puts (truths + falses).map(&:name).sort.join(',')
-    end
+    get_number('z')
   end
 end
+
+machine = Machine.new(input, logic)
+
+SHOULD = (machine.get_number('x') + machine.get_number('y'))
+
+current_swap = 
+(1..10).each do |i|
+  puts machine.get_output(i).upstream.count
+  if machine.compute((2 ** i) - 1, 1) != 2 ** i
+    puts machine.get_output(i)
+    puts machine.get_output(i).upstream.count
+    exit
+  end
+end
+
+# SHOULD.to_s(2).chars.map11(&:to_i).reverse.each_with_index do |bit, i|
+#   gate = GATES["z#{i.to_1s.rjust(2, '0')}"]
+#   tag = gate.get_output == bit ? :good : :bad
+#   gate.upstream.each { it.tags << tag }
+# end
